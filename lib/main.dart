a42bc09e8f1cd47a89551c885a8fc84520f1bf83
ADARSHKAKKATT <adarshvinayan@gmail.com>
2022-12-10 10:13:17 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:navigation/reg.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: true,
+    home:const Form() ,
+  ),
+  );
+}
+
+class Form extends StatefulWidget {
+  const Form({Key? key}) : super(key: key);
+
+  @override
+  State<Form> createState() => _FormState();
+}
+
+class _FormState extends State<Form> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        backgroundColor: Colors.lightGreen,
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 120,),
+              Icon(Icons.person_rounded,size: 90),
+              SizedBox(height: 40,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 80.0),
+                child: TextField(decoration: InputDecoration(
+                    label: Text("email"),
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                        Icons.mail
+                    )
+                ),
+                ),
+              ),
+              SizedBox(height: 40,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 80.0),
+                child: TextField(decoration: InputDecoration(
+                    label: Text("password"),
+                    border: OutlineInputBorder(),
+                    suffixIcon: Icon(
+                        Icons.remove_red_eye
+                    )
+                ),
+                  obscureText: true,
+                ),
+              ),
+              SizedBox(height: 30,),
+              ElevatedButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>const Reg()));
+              },child: Text("SUBMIT"),
+                style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  backgroundColor: Colors.indigo,
+                ),
+
+                /*TextButton(onPressed:(){}, child: Text("Submit"),
+              )*/
+              ),
+            ],
+          ),
+        ),
+      );
+  }
+}
